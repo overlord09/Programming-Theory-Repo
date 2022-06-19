0c2cdb0750961b8388aa730528fcaa301ecc325d
overlord09 <fagulha09@hotmail.com>
2022-06-19 20:54:28 +0100

    enemy patrol
    enemy can patrol all around

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : Movement
+{
+    [SerializeField] Transform[] areas;
+    int destinationPatrol;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance<0.2f)
+        {
+            Move(areas[destinationPatrol].position);//inheritance
+        }
+    }
+    protected override void Move(Vector3 m_destination)//polymorphism
+    {
+        base.Move(m_destination);
+        if (areas.Length > destinationPatrol) ;
+        destinationPatrol=(destinationPatrol+1)%areas.Length;
+    }
+}
