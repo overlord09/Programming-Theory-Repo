d0086ccf844cfaa7087e5043e5239f4e6251bab0
overlord09 <fagulha09@hotmail.com>
2022-07-11 18:13:17 +0100

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,14 +9,14 @@ public class Movement : MonoBehaviour
     float speed = 10;
     float rotationSpeed = 100;
     protected NavMeshAgent agent;
-    protected virtual void Move(float x,float z)
+    protected virtual void Move(float x,float z)//polimorphism
     {
 
 
         transform.Translate(Vector3.forward * z * speed * Time.deltaTime);
         transform.Rotate(Vector3.up * x * rotationSpeed * Time.deltaTime);
     }
-    protected virtual void Move(Vector3 m_destination)
+    protected virtual void Move(Vector3 m_destination)//polymorphism
     {
         GetComponent<NavMeshAgent>().destination = m_destination;
     }
